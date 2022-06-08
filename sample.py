8dc04adcc14d8875891afc8eb265c890d641652f
John Fabry <57239149+jfabry-noc@users.noreply.github.com>
2022-06-08 19:59:43 -0400

    Adds basic script.

--- sample.py
+++ sample.py
@@ -1,1 +1,16 @@
+import json
+import requests
 
+def main():
+    ip_request = requests.get("https://ip.borked.sh")
+    if ip_request.status_code == 200:
+        ip_dict = json.loads(ip_request.content)
+        print(ip_dict.get('ip'))
+    else:
+        print(f"Error: Response code: {ip_request.status_code}")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception (e):
+        print(e)
